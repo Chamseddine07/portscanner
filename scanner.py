257d30b581e98e1395197bb1bc9e8ac86b2656c9
Chamseddine07 <chamseddinegharbi07@gmail.com>
2026-04-10 15:31:24 +0200

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -4,22 +4,20 @@ PORT_NAMES = {
     21: "FTP",
     22: "SSH",
     23: "Telnet",
-    53: "DNS",
     80: "HTTP",
     443: "HTTPS",
     445: "SMB",
     3389: "RDP"
 }
 
-target = input("Enter IP or hostname to scan: ")
-ip = socket.gethostbyname(target)  
-print(f"Scanning {target} ({ip})\n")
 
 for port in PORT_NAMES:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(0.5)
 
-    status = s.connect_ex((ip, port))
+    status = s.connect_ex(("127.0.0.1", port))
+
+
 
     if status == 0:
         print(f"{port} IS OPEN. SERVICE: {PORT_NAMES[port]}")
